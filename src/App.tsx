4d8e70e2f95f9deade2128d27c63830d190d3617
mg901 <mgreen097@gmail.com>
2019-03-19 23:45:53 +0300

    refactor: fix components

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,9 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 
 export const App = () => (
   <div className="App">
     <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        Edit <code>src/App.tsx</code> and save to reload.
-      </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
+      <h1>Hello World</h1>
     </header>
   </div>
 );
