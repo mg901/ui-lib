869ac92c7e86366b5c9273c8f349ba2223ab6c59
mg901 <mgreen097@gmail.com>
2019-03-20 14:38:15 +0300

    chore(app): rename file

--- src/foo.tsx
+++ src/foo.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export const App = () => (
+export const App: React.SFC<{}> = (): JSX.Element => (
   <div className="App">
     <header className="App-header">
       <h1>Hello World</h1>
